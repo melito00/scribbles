9c8b652ccac8d932493a4eb00ffa61cacae2fbf8
Kenji Yamada <kenji.yamada@gmail.com>
2017-12-01 10:37:18 +0000

    add Set-Alias comment to PSStartupFile

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,8 +1,12 @@
 Import-Module PSReadLine
-Set-PSReadlineOption -EditMode Emacs
+Set-PSReadlineOption -EditMode Emacs -BellStyle None
 
 pushd c:\z\home\keyamada\
 
 Import-Module posh-git
 Start-SshAgent
 ssh-add c:\z\home\keyamada\.ssh\id_ed25519-2
+
+# Alias
+# Set-Alias -Name "alias name" -Value "original command"
+# Remove-Item alias:....
