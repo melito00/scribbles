a339be1e60c518b701de24cc59221bd3b9000808
Yamada Kenji <kenji.yamada@gmail.com>
2017-12-11 07:42:39 +0000

    add alias for notepad++

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -5,8 +5,14 @@ pushd c:\z\home\keyamada\
 
 Import-Module posh-git
 Start-SshAgent
-ssh-add c:\z\home\keyamada\.ssh\id_ed25519-2
+# ssh-add c:\z\home\keyamada\.ssh\id_ed25519
+
+# Functions
+function ll {
+  Get-ChildItem | Sort-Object
+}
 
 # Alias
 # Set-Alias -Name "alias name" -Value "original command"
 # Remove-Item alias:....
+Set-Alias -Name "np" -Value "notepad++"
