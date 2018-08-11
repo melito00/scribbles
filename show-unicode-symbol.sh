c68e2be1cf6a7dece853ab1793b2b04ad8d26082
Kenji Yamada <kenji.yamada@gmail.com>
2018-08-11 13:53:06 +0100

    change code point area

--- show-unicode-symbol.sh
+++ show-unicode-symbol.sh
@@ -31,12 +31,50 @@ for (( i=0x2580; i<0x2600; i++ )); do
     unichr $i
 done
 
+echo "\n"
+
+for (( i=0xe0a0; i<0xe0d5; i++ )); do
+    unichr $i
+done
+
+echo "\n"
+echo 'Font Awesome Extension'
 echo
-echo -ne '\u2581'; echo -n " | "; echo ▁
-echo -ne '\u2582'; echo -n " | "; echo ▂
-echo -ne '\u2583'; echo -n " | "; echo ▃
-echo -ne '\u2584'; echo -n " | "; echo ▄
-echo -ne '\u2585'; echo -n " | "; echo ▅
-echo -ne '\u2586'; echo -n " | "; echo ▆
-echo -ne '\u2587'; echo -n " | "; echo ▇
-echo -ne '\u2588'; echo -n " | "; echo █
+
+for (( i=0xe200; i<0xe2aa; i++ )); do
+    unichr $i
+done
+
+echo '\n'
+echo 'Seti-UI + Custom'
+echo
+
+for (( i=0xe5fa; i<0xe62c; i++ )); do
+    unichr $i
+done
+
+echo '\n'
+echo Devicons
+echo
+
+for (( i=0xe700; i<0xe7c6; i++ )); do
+    unichr $i
+done
+
+echo '\n'
+echo Font Awesome
+echo
+
+for (( i=0xf000; i<0xf2e1; i++ )); do
+    unichr $i
+done
+
+echo "\n"
+echo -n '\\u2581 : '; echo -ne '\u2581'; echo -n " | "; echo ▁
+echo -n '\\u2582 : '; echo -ne '\u2582'; echo -n " | "; echo ▂
+echo -n '\\u2583 : '; echo -ne '\u2583'; echo -n " | "; echo ▃
+echo -n '\\u2584 : '; echo -ne '\u2584'; echo -n " | "; echo ▄
+echo -n '\\u2585 : '; echo -ne '\u2585'; echo -n " | "; echo ▅
+echo -n '\\u2586 : '; echo -ne '\u2586'; echo -n " | "; echo ▆
+echo -n '\\u2587 : '; echo -ne '\u2587'; echo -n " | "; echo ▇
+echo -n '\\u2588 : '; echo -ne '\u2588'; echo -n " | "; echo █
