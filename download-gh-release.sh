ad0ade174c04a0a8fabd84dedd4b7988c865a4c3
Yamada Kenji <kenji.yamada@gmail.com>
2024-03-28 11:04:03 +0900

    add: shell script, download files from github release

---
+++ download-gh-release.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+#
+
+function download_gh_release() {
+  echo Download $2 from github: $1
+  # curl -sLO $(curl -s https://api.github.com/repos/$1/releases/latest | jq -r '.assets[] | select(.name | startswith("'"$2"'")) | .browser_download_url')
+  download_url=$(curl -s https://api.github.com/repos/$1/releases/latest | jq -r '.assets[] | select(.name | startswith("'"$2"'")) | .browser_download_url')
+  echo ${download_url}
+  curl -sLO ${download_url}
+}
+
+download_gh_release yuru7/HackGen HackGen_NF
+download_gh_release yuru7/moralerspace MoralerspaceNF
+
