0dab1877d3380364b71e7551eca114c54439eb4b
melito00 <melito@jan.email.ne.jp>
2024-12-28 05:56:57 +0900

    update: use loop, download gh

--- download-gh-release.sh
+++ download-gh-release.sh
@@ -1,14 +1,21 @@
 #!/bin/bash
-#
 
 function download_gh_release() {
-  echo Download $2 from github: $1
+  local repo="$1"
+  local target="$2"
+  echo Download "${target}" from github: "${repo}"
   # curl -sLO $(curl -s https://api.github.com/repos/$1/releases/latest | jq -r '.assets[] | select(.name | startswith("'"$2"'")) | .browser_download_url')
-  download_url=$(curl -s https://api.github.com/repos/$1/releases/latest | jq -r '.assets[] | select(.name | startswith("'"$2"'")) | .browser_download_url')
+  download_url=$(curl -s "https://api.github.com/repos/${repo}/releases/latest" | jq -r '.assets[] | select(.name | startswith("'"${target}"'")) | .browser_download_url')
   echo ${download_url}
   curl -sLO ${download_url}
 }
 
-download_gh_release yuru7/HackGen HackGen_NF
-download_gh_release yuru7/moralerspace MoralerspaceNF
+#download_gh_release yuru7/HackGen HackGen_NF
+#download_gh_release yuru7/moralerspace MoralerspaceNF
 
+while IFS=',' read -r key value; do
+  download_gh_release "$key" "$value"
+done <<EOF
+yuru7/HackGen,HackGen_NF
+yuru7/moralerspace,MoralerspaceNF
+EOF
